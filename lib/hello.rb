5e41844e09916a4685d9505243fa6c3636172404
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 19:06:21 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(arr)
+  if block_given?
+    i=0 
+    while i<arr.size 
+     yield arr[i]
+    i+=1 
+    end 
+    return arr 
+  else 
+    puts"Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
